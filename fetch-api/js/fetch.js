989cd940f0efdeb529f8baddadf3219d0c98e597
simonwhiteQA <simonwhite17@icloud.com>
2021-07-23 10:39:36 +0100

    fetch

--- fetch-api/js/fetch.js
+++ fetch-api/js/fetch.js
@@ -1,16 +1,68 @@
-(() => {
+//GET request for 'List User'
+fetch('https://reqres.in/api/users?page=2')
+.then((response) => {
+    if (response.status !== 200) {
+        console.log(`Looks like there was a problem.Status Code: ${ response.status }`);
+        return;
+    }
+response.json()
+.then(data => console.log(data));         
+})
+.catch(err => console.error(`Fetch Error :-S ${err}`));
 
-let container = document.querySelector("#container");
+//GET request for 'Single User' with the id of 2
+fetch('https://reqres.in/api/users/2')
+.then((response) => {
+    if (response.status !== 200) {
+        console.log(`Looks like there was a problem.Status Code: ${ response.status }`);
+        return;
+    }
+response.json()
+.then(data => console.log(data));         
+})
+.catch(err => console.error(`Fetch Error :-S ${err}`));
 
-function fetch(id) {
+//POST request for 'Create'
+fetch("https://reqres.in/api/users", {
+         method: 'post',
+         headers: {
+             "Content-type": "application/json"
+         },
+         body: JSON.stringify({
+            name: "Morpheus",
+            job: "Leader"
+           })
+         })
+         .then(res => res.json())
+         .then(data => console.log(`Request succeeded with JSON response ${data}`))
+         .catch(error => console.error(`Request failed ${error}`));
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
-    .finally(() => { console.log("all done"); });
-}
+//POST request for 'Register - Successful'
+fetch("https://reqres.in/api/register", {
+     method: 'post',
+     headers: {
+         "Content-type": "application/json"
+     },
+     body: JSON.stringify({
+         email: "eve.holt@reqres.in",
+         password: "pistol"
+       })
+     })
+     .then(res => res.json())
+     .then(data => console.log(`Request succeeded with JSON response ${data}`))
+     .catch(error => console.error(`Request failed ${error}`));
 
-fetch(8);
-
-})();
+//POST request for 'Login - Successful'
+fetch("https://reqres.in/api/login", {
+    method: 'post',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+        email: "eve.holt@reqres.in",
+        password: "cityslicka"
+      })
+  })
+  .then(res => res.json())
+  .then(data => console.log(`Request succeeded with JSON response ${data}`))
+  .catch(error => console.error(`Request failed ${error}`));
